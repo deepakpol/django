1c49d52a9ebe822402ad5da3d67b78d47599fd9c
deepakpol <deepakpol@gmail.com>
2026-06-12 15:24:06 -0400

    AAVA generated tests for SDD-87

--- tests/utils_tests/test_text.py
+++ tests/utils_tests/test_text.py
@@ -8,6 +8,7 @@ from django.utils import text
 from django.utils.functional import lazystr
 from django.utils.text import format_lazy
 from django.utils.translation import gettext_lazy, override
+from django.utils.text import slugify
 
 IS_WIDE_BUILD = len("\U0001f4a9") == 1
 
@@ -443,3 +444,31 @@ class TestUtilsText(SimpleTestCase):
         )
         with override("fr"):
             self.assertEqual("Ajout de article «\xa0My first try\xa0».", s)
+
+    def test_spaces_become_hyphens_and_lowercase(self):
+        """
+        Test that spaces become single hyphens and text is lowercased.
+        Given 'Hello World Foo', when slugify is applied, 
+        then the result is 'hello-world-foo'.
+        """
+        result = slugify('Hello World Foo')
+        self.assertEqual(result, 'hello-world-foo')
+
+    def test_disallowed_punctuation_stripped(self):
+        """
+        Test that disallowed punctuation is stripped from the input.
+        Given 'C++ & Python: A Guide!', when slugify is applied,
+        then punctuation is removed and the result is 'c-python-a-guide'.
+        """
+        result = slugify('C++ & Python: A Guide!')
+        self.assertEqual(result, 'c-python-a-guide')
+
+    def test_unicode_transliterated_to_ascii_by_default(self):
+        """
+        Test that Unicode is transliterated to ASCII by default.
+        Given 'Café Déjà Vu' with accents and allow_unicode=False,
+        when slugify is applied, then the result is 'cafe-deja-vu'.
+        """
+        result = slugify('Café Déjà Vu', allow_unicode=False)
+        self.assertEqual(result, 'cafe-deja-vu')
+
